c9c0d2d91915f3482780fa06c2847191ee19efd6
Phan Thao <phanthao550@gmail.com>
2025-01-13 21:38:49 +0700

    1234

---
+++ fastfoodapp/lib/presentation/states/resultsearchviewmodel.dart
@@ -0,0 +1,83 @@
+import 'package:fastfoodapp/data/models/ResultSearchModel.dart';
+import 'package:fastfoodapp/data/repositories/ProductRepository.dart';
+import 'package:flutter/material.dart';
+
+class Resultsearchviewmodel extends ChangeNotifier {
+  final Productrepository _productrepository;
+  Resultsearchviewmodel(this._productrepository);
+
+  final TextEditingController _keywordTextcontroller = TextEditingController();
+  String selectedNewest = "GẦN ĐÂY";
+  String selectedPrice = "GIÁ";
+  bool isRelevantSelected = false;
+
+  TextEditingController get keywordTextcontroller => _keywordTextcontroller;
+
+  List<Resultsearchmodel> listProduct = [];
+  List<Resultsearchmodel> filteredProducts = [];
+
+  Future<void> SearchProductByName(String keyword) async {
+    List<Resultsearchmodel> response =
+        await _productrepository.SearchProductByName(keyword);
+    listProduct = response;
+    applyFilters();
+  }
+
+  // void updateNewest(String value) {
+  //   selectedNewest = value;
+  //   notifyListeners();
+  // }
+
+  // void applyPriceFilter(String value) {
+  //   if (value == "35k - 100k") {
+  //     listProduct = listProduct.where((item) => (item.price > 34 && item.price < 101)).toList();
+  //   } else if (value == "100k - 200k") {
+  //    listProduct = listProduct.where((item) => (item.price > 100 && item.price < 201))
+  //         .toList();
+  //   } else {
+  //     listProduct = listProduct.where((item) => (item.price > 200)).toList();
+  //   }
+  //   notifyListeners();
+  // }
+
+  void updatePrice(String value) {
+    selectedPrice = value;
+    applyFilters();
+  }
+
+  void toggleRelevant() {
+    isRelevantSelected = !isRelevantSelected;
+    applyFilters();
+    notifyListeners();
+  }
+
+  void applyFilters() {
+    filteredProducts = listProduct.where((product) {
+      bool matchPrice = selectedPrice == "GIÁ" ||
+          (selectedPrice == '35k - 100k' &&
+              product.price >= 35 &&
+              product.price <= 100) ||
+          (selectedPrice == '100k - 200k' &&
+              product.price > 100 &&
+              product.price <= 200) ||
+          (selectedPrice == '>200k' && product.price > 200);
+      return matchPrice;
+    }).toList();
+
+    if (isRelevantSelected) {
+      sortByRelevance(); // Sắp xếp theo tiêu chí liên quan
+    }
+    notifyListeners();
+  }
+
+  void sortByRelevance() {
+    filteredProducts.sort((a, b) =>
+        a.productName.toLowerCase().compareTo(b.productName.toLowerCase()));
+  }
+
+  @override
+  void dispose() {
+    _keywordTextcontroller.dispose();
+    super.dispose();
+  }
+}
---
+++ fastfoodapp/lib/presentation/widgets/iteminresultsearch.dart
@@ -0,0 +1,130 @@
+import 'package:fastfoodapp/res/colors.dart';
+import 'package:fastfoodapp/res/size.dart';
+import 'package:fastfoodapp/res/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sizer/sizer.dart';
+
+class ItemInResultSearchScreen extends StatelessWidget {
+  const ItemInResultSearchScreen({
+    super.key,
+    required this.foodName,
+    required this.price,
+    required this.address,
+    required this.image,
+    required this.shopName,
+    required this.onTap,
+  });
+
+  final String image;
+  final String foodName;
+  final String price;
+  final String address;
+  final String shopName;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.sp, vertical: 5.sp),
+        decoration: BoxDecoration(
+          color: Colors.white, // Màu nền của container
+          borderRadius: BorderRadius.circular(10), // Bo góc
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1), // Màu và độ mờ của bóng
+              blurRadius: 6, // Độ mờ của bóng
+              offset: const Offset(0, 4), // Độ dịch chuyển bóng (x, y)
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 47.sp,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10), // Bo góc cho ảnh
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+            SizedBox(height: 10.sp),
+            // Nội dung thông tin
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.sp),
+              child: Expanded(
+                ///flex: 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Tên món ăn
+                    Text(
+                      foodName,
+                      style: StylesOfWidgets.textStyle1(
+                          clr: Colors.black, fs: SizeOfWidget.sizeOfH1),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 10.sp),
+                    // Địa chỉ
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.location_on,
+                            size: 18.sp, color: AppColors.gray),
+                        const SizedBox(width: 4.0),
+                        Expanded(
+                          child: Text(
+                            address,
+                            style: StylesOfWidgets.textStyle1(
+                                clr: AppColors.gray, fs: SizeOfWidget.sizeOfH3),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10.sp),
+                    // Tên cửa hàng
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.storefront_outlined,
+                            size: 18.sp, color: AppColors.gray),
+                        const SizedBox(width: 4.0),
+                        Expanded(
+                          child: Text(
+                            shopName,
+                            style: StylesOfWidgets.textStyle1(
+                                clr: AppColors.gray, fs: SizeOfWidget.sizeOfH3),
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20.sp),
+                    // Giá tiền (vị trí góc dưới phải)
+                    Text(
+                      '$price đ',
+                      style: StylesOfWidgets.textStyle1(
+                          clr: AppColors.primaryColor,
+                          fs: SizeOfWidget.sizeOfH1,
+                          fw: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
